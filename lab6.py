a3d94cb04d93f43ea2dcf3531b54acefba7acd4e
asmita91 <katelasmita91@gmail.com>
2021-11-29 17:41:12 +0545

    new programs added

--- lab6.py
+++ lab6.py
@@ -1,8 +1,4 @@
 #program to find sum of first 10 even numbers
 inp1=range(2,22,2)
 sum1=sum(inp1)
-print(sum1)
-#program to find sum of first 10 odd numbers
-inp2=range(1,21,2)
-sum2=sum(inp2)
-print(sum2)
+print(f"the sum of first ten even numbers is {sum1}")
