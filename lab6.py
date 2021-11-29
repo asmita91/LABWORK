a414e2238fc11e8993e8370e3814fbe640625cea
asmita91 <katelasmita91@gmail.com>
2021-11-29 13:28:26 +0545

    added

---
+++ lab6.py
@@ -0,0 +1,8 @@
+#program to find sum of first 10 even numbers
+inp1=range(2,22,2)
+sum1=sum(inp1)
+print(sum1)
+#program to find sum of first 10 odd numbers
+inp2=range(1,21,2)
+sum2=sum(inp2)
+print(sum2)
---
+++ lab7.py
@@ -0,0 +1,4 @@
+#program to find sum of first 10 odd numbers
+inp2=range(1,21,2)
+sum2=sum(inp2)
+print(sum2)
