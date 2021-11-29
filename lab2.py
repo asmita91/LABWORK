85569d24970b93156a8591faf789b46016104776
asmita91 <katelasmita91@gmail.com>
2021-11-29 13:02:07 +0545

    lab work committed

---
+++ lab2.py
@@ -0,0 +1,5 @@
+ #program to find area
+inp1=float(input("enter the base of traingle in cm::"))
+inp2=float(input("enter the height of traingle in cm::"))
+area=(1/2*inp1*inp2)
+print(f"the area is::{area} cm\u00b2")
---
+++ lab3.py
@@ -0,0 +1,3 @@
+inp1=float(input("enter the length of cube=="))
+v=(inp1**3)
+print(f"the volume of the given cube is {v}cm \u00b3")
