fb5ca70cdc61620eb4523bd507096d972c1f6aee
asmita91 <katelasmita91@gmail.com>
2021-12-06 12:24:51 +0545

    corrected

--- day3/lab9.py
+++ day3/lab9.py
@@ -1,8 +1,10 @@
 #program to take password and check if it is correct or not
+uname=input("enter your username")
+pwd=input("enter your password")
 for i in range(3):
-    uname=input("enter the user name::")
-    pwd=input("enter your password")
-    if uname == 'asmita91' and pwd == 'lifeisgood':
+    uname1=input("enter the user name::")
+    pwd1=input("enter the password")
+    if uname ==uname1 and pwd == pwd1:
         print("Logged in Successfully")
         break
     else:
